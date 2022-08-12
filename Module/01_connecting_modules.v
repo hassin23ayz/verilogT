cb6ad220e2d8b7b72b8891494316fededdd5f731
hassin23ayz <hassin23ayz@gmail.com>
2022-08-12 12:28:02 +0600

    todo gtkwave for verilog

--- Module/01_connecting_modules.v
+++ Module/01_connecting_modules.v
@@ -1,3 +1,18 @@
 // A Module can contain instances of other modules 
 // And this is how a larger complex circuits are designed 
 
+module mod_a (
+    input in1, in2,
+    output out,
+    );
+
+endmodule
+
+
+module top_module (
+    input a,b,
+    output out
+    );
+
+    
+endmodule
