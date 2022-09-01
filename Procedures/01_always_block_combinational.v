5de16b7fa32d237272298d46b146aca609b8c6dc
hassin23ayz <hassin23ayz@gmail.com>
2022-09-01 10:23:52 +0600

    same behaviour by assign and always block of procedures

---
+++ Procedures/01_always_block_combinational.v
@@ -0,0 +1,15 @@
+// Build an AND gate using both an assign statement and a combinational always block.
+
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a & b;
+    
+    always @(*) begin
+        out_alwaysblock = a & b;
+    end
+
+endmodule
